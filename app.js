3bf2d49d0469db6ec4f8894bad616f11efded9ba
miniwangdali <wang__dali@sina.com>
2016-12-21 08:09:52 +0800

    change back to original version

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var mongoose = require('mongoose');
 
 var Articles = require('./article');
 
-// var hostname = '127.0.0.1';
-var port = 80;
+var hostname = '127.0.0.1';
+var port = 3001;
 
 var app = express();
 var dataRouter;
@@ -81,9 +81,9 @@ app.get('/', function(req, res, next) {
 app.use('/data',dataRouter);
 app.use(express.static(__dirname));
 
-// app.listen(port, hostname, function(){
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-app.listen(port, function(){
+app.listen(port, hostname, function(){
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+// app.listen(port, function(){
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
